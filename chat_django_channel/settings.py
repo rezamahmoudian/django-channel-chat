4f48d4e5fa9bfbb26a5fc5ef9ae25bda8bd4d977
Reza Mahmoudian <rezamahmoudian80@gmail.com>
2023-07-19 13:49:20 +0330

    install daphne and start app chat

--- chat_django_channel/settings.py
+++ chat_django_channel/settings.py
@@ -31,12 +31,15 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "daphne",
+    'chat',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
 ]
 
 MIDDLEWARE = [
@@ -122,3 +125,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+ASGI_APPLICATION = "myproject.asgi.application"
